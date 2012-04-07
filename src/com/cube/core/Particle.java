375c09b773542edba522ccd12d8d5cb253786cdc
Drew Malin <drewmalin@gmail.com>
2012-04-07 15:00:37 -0700

    Changes to make 'Particle' use arrays instead of singular floats/ints.

--- src/com/cube/core/Particle.java
+++ src/com/cube/core/Particle.java
@@ -2,6 +2,12 @@ package com.cube.core;
 
 public class Particle {
 	
+	public float position[];
+	public float movement[];
+	public float maximum[];
+	public float scale[];
+	public float color[];
+	/*
 	public float xPos;
 	public float yPos;
 	public float zPos;
@@ -21,8 +27,16 @@ public class Particle {
 	public float xScale;
 	public float yScale;
 	public float zScale;
-	
+	*/
 	public float direction;
 	public float acceleration;
 	public float deceleration;
+	
+	public Particle() {
+		position = new float[3];
+		movement = new float[3];
+		maximum = new float[3];
+		scale = new float[3];
+		color = new float[3];
+	}
 }
